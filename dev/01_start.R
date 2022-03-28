c659cd6cbb864887e6975bb5b678b4f1d5a17740
StefrodopCodes94 <stef.rodop94@gmail.com>
2022-03-28 07:47:08 +0000

    App setup

--- dev/01_start.R
+++ dev/01_start.R
@@ -22,9 +22,9 @@ golem::fill_desc(
   pkg_name = "myshinyapp", # The Name of the package containing the App
   pkg_title = "PKG_TITLE", # The Title of the package containing the App
   pkg_description = "PKG_DESC.", # The Description of the package containing the App
-  author_first_name = "AUTHOR_FIRST", # Your First Name
-  author_last_name = "AUTHOR_LAST", # Your Last Name
-  author_email = "AUTHOR@MAIL.COM", # Your Email
+  author_first_name = "Stefanos", # Your First Name
+  author_last_name = "Rodopoulos", # Your Last Name
+  author_email = "Stef.rodop94@gmail.com", # Your Email
   repo_url = NULL # The URL of the GitHub Repo (optional)
 )
 
